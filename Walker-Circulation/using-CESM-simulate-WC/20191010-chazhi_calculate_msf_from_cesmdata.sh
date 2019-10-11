51180400d4e6842256281c0da7bd22dfc4ed77b6
linshh <linshh55@mail2.sysu.edu.cn>
2019-10-12 04:01:03 +0800

    191012

--- Walker-Circulation/using-CESM-simulate-WC/20191010-chazhi_calculate_msf_from_cesmdata.sh
+++ Walker-Circulation/using-CESM-simulate-WC/20191010-chazhi_calculate_msf_from_cesmdata.sh
@@ -17,36 +17,40 @@
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
 PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F_1850-PDAY/
+
 STEP=3
-echo test 
-#step1 : merge the cesm data into a whole data by using cdo
-  ## the prefix of data is usually CESM compet name ,alarm for time select
-if  [ $STEP == 3 ] ; then
-  echo test
-    # cd $PRE_DIR_ORG 
-    # cdo select,name=U,V,OMEGA,PRECL,PSL,PS F_1850-PDAY.cam.h0.* F_1850-PDAY.cam.h0.197901-201212.nc 
-    # echo "merge cesm data done"
+# step1 : merge the cesm data into a whole data by using cdo
+  ### the prefix of data is usually CESM compet name ,alarm for time select
+if  [ ! -e  ${PRE_DIR_ORG}F_1850-PDAY.cam.h0.197901-201212.nc ] ; then
+    echo "don't exit merge file, procecing..."
+    cd $PRE_DIR_ORG
+    rm F_1850-PDAY.cam.h0.197901-201212.nc
+    cdo select,name=U,V,OMEGA,PRECL,PSL,PS F_1850-PDAY.cam.h0.* F_1850-PDAY.cam.h0.197901-201212.nc 
 fi
 
-#step2 : interpolate the data from hybird level to pressure level
- 
-if  [ $STEP == 3 ] ; then
+# step2 : interpolate the data from hybird level to pressure level
+  if [ ! -e ${PRE_DIR_ORG}F_1850-PDAY.cam.h0.197901-201212.nc ] ; then    ####判断差值的文件是否已经存在
+    echo "don't exit chazhi file, procecing..."
     cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/ 
-    ncl  inpath='"~/lsh/CESM-data/f19_f19_FAMIP_rmindian/f19_f19_FAMIP_rmindian.cam.h0.197901-200611.nc"' \
-      outpath='"~/lsh/CESM-data/f19_f19_FAMIP_rmindian/f19_f19_FAMIP_rmindian.cam.h0.197901-200611_chazhi.nc"' \
-       ./2019925-CESM-data-chazhi.ncl 
-   echo "finish step2"
-fi
+    ncl  inpath='"~/lsh/CESM-data/F_1850-PDAY/F_1850-PDAY.cam.h0.197901-201212.nc"' \
+         outpath='"~/lsh/CESM-data/F_1850-PDAY/F_1850-PDAY.cam.h0.197901-201212_chazhi.nc"' \
+       ./2019925-CESM-data-chazhi.ncl
+    echo "finish CESM chazhi"
+  fi
 # step3 calculate mass stream function
 
-if  [ $STEP == 3 ] ; then
+  if  [ ! -e /home/lsh/CESM-data/F_1850-PDAY/F_1850-PDAY.cam.h0.197901-201212_chazhi.nc ] ; then
      ## 斜杠用来添加"，不然"会被默认成输出变量用的符号 
-   ncl path=\"${PRE_DIR_ORG}\"               \
-       filename='"F_1850-PDAY.cam.h0.197901-201212.nc"' \
-     ./2019926-mass_streamfunciton_cesm.ncl 
-   echo "finish step3"
-fi
+   echo "don't exit msf file, procecing..."
+   ncl inpath=\"${PRE_DIR_ORG}\"               \
+       filename='"F_1850-PDAY.cam.h0.197901-201212_chazhi.nc"' \
+       outputpath='"~/lsh/data/wc-reslut/"' \
+       outputname='"msf_F_1850-PDAY_1979-201212.nc"' \
+     ./2019926-mass_streamfunciton_cesm.ncl
+   echo "finish CESM mass_stream function"
+  fi
 
+#-----------------------------------------------------------
 
 
 
