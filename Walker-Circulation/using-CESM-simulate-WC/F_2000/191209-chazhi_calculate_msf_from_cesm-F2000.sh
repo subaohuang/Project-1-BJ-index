af5119ec2092f9814f865fcdb1c56d6824479579
linshh <linshh55@mail2.sysu.edu.cn>
2019-12-13 04:01:05 +0800

    191213

--- Walker-Circulation/using-CESM-simulate-WC/F_2000/191209-chazhi_calculate_msf_from_cesm-F2000.sh
+++ Walker-Circulation/using-CESM-simulate-WC/F_2000/191209-chazhi_calculate_msf_from_cesm-F2000.sh
@@ -16,12 +16,12 @@
 
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
-# PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_addindianwarming/
-PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_CTRL/
+# PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_CTRL/
+PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_addatlanticwarming/
 
 STEP=3
-modelname=CTRL
-
+modelname=F_2000_atlantic
+variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3
 
 
 # step1 : merge the cesm data into a whole data by using cdo
@@ -31,7 +31,7 @@ modelname=CTRL
     echo "don't exit merge file, procecing..."
     cd $PRE_DIR_ORG
     rm ${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012.nc
-    cdo select,name=U,V,OMEGA,PRECL,PRECC,PSL,PS ${modelname}.cam.h0.* ${modelname}.cam.h0.0101-4012.nc
+    cdo select,name=${variable} ${modelname}.cam.h0.* ${modelname}.cam.h0.0101-4012.nc
   fi
 
 # step2 : interpolate the data from hybird level to pressure level
@@ -45,7 +45,7 @@ modelname=CTRL
   fi 
 # step3 calculate mass stream function
 
-  if  [ ! -e /home/lsh/data/wc-result/msf_${modelname}_0101-4012.nc ] ; then
+  if  [ ! -e /home/ys17-19/lsh/data/wc-result/msf_${modelname}_0101-4012.nc ] ; then
      ## 斜杠用来添加"，不然"会被默认成输出变量用的符号 
    echo "don't exit msf file, procecing..."
    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
@@ -56,7 +56,8 @@ modelname=CTRL
      ./191209-mass_streamfunciton_cesm-F2000.ncl
    echo "finish CESM mass_stream function"
   fi
-
+  
+   echo "finish this script"
 #-----------------------------------------------------------
 
 
