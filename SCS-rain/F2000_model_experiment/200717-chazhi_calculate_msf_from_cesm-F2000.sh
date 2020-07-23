13414050afb11ced466701302f18a0da4a84615b
linshh <linshh55@mail2.sysu.edu.cn>
2020-07-24 04:03:09 +0800

    200724

--- SCS-rain/F2000_model_experiment/200717-chazhi_calculate_msf_from_cesm-F2000.sh
+++ SCS-rain/F2000_model_experiment/200717-chazhi_calculate_msf_from_cesm-F2000.sh
@@ -17,11 +17,11 @@
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_IPO/
-PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_addatlanticwarimg_spring_notest/
+PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_addatlanticwarimg_winter_notest/
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_addallocean_tropical/
 
 STEP=3
-modelname=F_2000_atl_spring_notest
+modelname=F_2000_atl_winter_notest
 variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q
 
 
