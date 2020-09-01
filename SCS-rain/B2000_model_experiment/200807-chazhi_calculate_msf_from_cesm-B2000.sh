7f2d56c7be6150f87b335b17a52ff2ff475405da
linshh <linshh55@mail2.sysu.edu.cn>
2020-09-02 04:03:08 +0800

    200902

--- SCS-rain/B2000_model_experiment/200807-chazhi_calculate_msf_from_cesm-B2000.sh
+++ SCS-rain/B2000_model_experiment/200807-chazhi_calculate_msf_from_cesm-B2000.sh
@@ -18,11 +18,12 @@
 # Caution: DO NOT DELETE /" IN STRING!
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_IPO/
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/B/B2000_alt_north_year_new/
-PRE_DIR_ORG=/home/yangsong3/data-model/lsh/CESM/B/B2000_alt_north_autumn
+PRE_DIR_ORG=/home/yangsong3/data-model/lsh/CESM/B/B2000_alt_north_winter/
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_addallocean_tropical/
 
 STEP=3
-modelname=B2000_alt_north_autumn
+modelname=B2000_alt_north_winter
+
 variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q
 
 
