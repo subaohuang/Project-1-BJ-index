a612c5833deb67d818ffff9286d1b1b5b17ecb8f
linshh <linshh55@mail2.sysu.edu.cn>
2020-09-10 04:03:08 +0800

    200910

--- SCS-rain/F2000_model_experiment/200717-chazhi_calculate_msf_from_cesm-F2000.sh
+++ SCS-rain/F2000_model_experiment/200717-chazhi_calculate_msf_from_cesm-F2000.sh
@@ -17,10 +17,10 @@
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_IPO/
-PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_CTRL_new/
+PRE_DIR_ORG=//home/ys17-19/lsh/CESM-data/F/F_2000_addatlanticwarimg_winter_notest/
 
 STEP=3
-modelname=F_2000_CTRL
+modelname=F_2000_atl_winter_notest
 variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q
 
 
@@ -58,21 +58,35 @@ variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q
 #      ./191209-mass_streamfunciton_cesm-F2000.ncl
 #    echo "finish CESM mass_stream function"
 #   fi
-  # # step4 calculate Local WC and HC
 
-   if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012_local_wk_hc_500.nc ] ; then    ####判断差值的文件是否已经存在
-   echo "don't exit WC and HC file, procecing..."
-   cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
+
+# # # step4 calculate Local WC and HC
+#    if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012_local_wk_hc_500.nc ] ; then    ####判断差值的文件是否已经存在
+#    echo "don't exit WC and HC file, procecing..."
+#    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
+#    pwd
+#    ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
+#        filename=\"${modelname}.cam.h0.0101-4012_chazhi.nc\" \
+#        outputpath=\"${PRE_DIR_ORG}\"               \
+#        outputname=\"${modelname}.cam.h0.0101-4012_local_wk_hc_500.nc\" \
+#      ./200902-local_wk_hc_cesm-F2000.ncl
+#    echo "finish CESM mass_stream function"
+#   fi
+#    echo "finish this script"
+# #-----------------------------------------------------------
+
+
+# # step5 calculate vp and sf
+   if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012_vp_sf.nc ] ; then    ####判断差值的文件是否已经存在
+   echo "don't exit vp and sf file, procecing..."
+   cd /home/ys17-19/lsh/Project/SCS-rain/F2000_model_experiment/
    pwd
-   ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
-       filename=\"${modelname}.cam.h0.0101-4012_chazhi.nc\" \
-       outputpath=\"${PRE_DIR_ORG}\"               \
-       outputname=\"${modelname}.cam.h0.0101-4012_local_wk_hc_500.nc\" \
-     ./200902-local_wk_hc_cesm-F2000.ncl
-   echo "finish CESM mass_stream function"
+   ncl -nQ infilepath=\"${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012_chazhi.nc\" \
+       outfilepath=\"${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012_vp_sf.nc\"   \
+     ./200909-F2000-cal-write-era-velocity-potensial-streamfuc.ncl
+   echo "finish CESM VP and SF calculate"
   fi
    echo "finish this script"
 #-----------------------------------------------------------
-
 
 
