922999343662ab6317bcf058a8040988262cc216
linshh <linshh55@mail2.sysu.edu.cn>
2020-11-15 04:01:50 +0800

    201115

--- SCS-rain/F2000_model_experiment/200717-chazhi_calculate_msf_from_cesm-F2000.sh
+++ SCS-rain/F2000_model_experiment/200717-chazhi_calculate_msf_from_cesm-F2000.sh
@@ -17,9 +17,9 @@
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_IhPO/
-PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_WNP_spring/
+PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/FC5_WEP_summer/
 STEP=3
-modelname=F_2000_WNP_spring
+modelname=FC5_WEP_summer
 # variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,UBOT,VBOT,TREFHT
 variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,TREFHT
 
