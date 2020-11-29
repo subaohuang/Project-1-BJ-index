8a331c23d3a7e65667f0058077e76d2a6c0ef6e3
linshh <linshh55@mail2.sysu.edu.cn>
2020-11-30 04:03:09 +0800

    201130

--- SCS-rain/F2000_model_experiment/200727-cdo-merge-cesmdata-olr-heatflux.sh
+++ SCS-rain/F2000_model_experiment/200727-cdo-merge-cesmdata-olr-heatflux.sh
@@ -17,11 +17,11 @@
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_IPO/
-PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/FC5_WEP_winter/
+PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/FC5_ind_wep_spring/
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_addallocean_tropical/
 
 STEP=3
-modelname=FC5_WEP_winter
+modelname=FC5_ind_wep_spring
 variable=FLUT,FSNS,FLNS,LHFLX,SHFLX,U10
 
 
