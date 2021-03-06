2481f53f0f5e40e14aff038a68afeea3c2ed339d
linshh <linshh55@mail2.sysu.edu.cn>
2021-03-07 04:03:10 +0800

    210307

--- Walker-Circulation/using-CESM-simulate-WC/F_2000/200717-chazhi_calculate_msf_from_cesm-F2000.sh
+++ Walker-Circulation/using-CESM-simulate-WC/F_2000/200717-chazhi_calculate_msf_from_cesm-F2000.sh
@@ -17,50 +17,50 @@
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_IhPO/
-PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/FC5_WNP_spring/
+PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_CTRL_new/
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_GHG/
 STEP=3
-modelname=FC5_WNP_spring
+modelname=F_2000_CTRL
 # modelname=F_2000_ghg
 # variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,UBOT,VBOT,TREFHT
 variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,TREFHT
 # variable=hyam
 
 
-#step1 : merge the cesm data into a whole data by using cdo
-  ## the prefix of data is usually CESM compet name ,alarm for time select
+# #step1 : merge the cesm data into a whole data by using cdo
+#   ## the prefix of data is usually CESM compet name ,alarm for time select
 
-  if  [ ! -e  ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc ] ; then
-    echo "don't exit merge file, procecing..."
-    cd $PRE_DIR_ORG
-    rm ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc
-    cdo_old select,name=${variable} ${modelname}.cam.h0.* ${modelname}.cam.h1.0101-4012.nc
-  fi
-
-# step2 : interpolate the data from hybird level to pressure level
-  if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc ] ; then    ####判断差值的文件是否已经存在
-    echo "don't exit chazhi file, procecing..."
-    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
-    pwd
-    ncl  -nQ inpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc\" \
-         outpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc\" \
-       /home/ys17-19/lsh/Project/SCS-rain/annual/191209-CESM-data-chazhi.ncl
-    echo "finish CESM chazhi"
-  fi 
-# # step3 calculate mass stream function
-
-#   if  [ ! -e /home/ys17-19/lsh/data/wc-result/msf_${modelname}_0101-4012.nc ] ; then
-#      ## 斜杠用来添加"，不然"会被默认成输出变量用的符号 
-#    echo "don't exit msf file, procecing..."
-#    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
-#    pwd
-#    ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
-#        filename=\"${modelname}.cam.h0.0101-4012_chazhi.nc\" \
-#        outputpath='"~/lsh/data/wc-result/"' \
-#        outputname=\"msf_${modelname}_0101-4012.nc\" \
-#      ./191209-mass_streamfunciton_cesm-F2000.ncl
-#    echo "finish CESM mass_stream function"
+#   if  [ ! -e  ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc ] ; then
+#     echo "don't exit merge file, procecing..."
+#     cd $PRE_DIR_ORG
+#     rm ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc
+#     cdo_old select,name=${variable} ${modelname}.cam.h0.* ${modelname}.cam.h1.0101-4012.nc
 #   fi
+
+# # step2 : interpolate the data from hybird level to pressure level
+#   if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc ] ; then    ####判断差值的文件是否已经存在
+#     echo "don't exit chazhi file, procecing..."
+#     cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
+#     pwd
+#     ncl  -nQ inpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc\" \
+#          outpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc\" \
+#        /home/ys17-19/lsh/Project/SCS-rain/annual/191209-CESM-data-chazhi.ncl
+#     echo "finish CESM chazhi"
+#   fi 
+# step3 calculate mass stream function
+
+  if  [ ! -e /home/yangsong3/data-observation/linshh/data/wc-result/msf_${modelname}_0101-4012.nc ] ; then
+     ## 斜杠用来添加"，不然"会被默认成输出变量用的符号 
+   echo "don't exit msf file, procecing..."
+   cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
+   pwd
+   ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
+       filename=\"${modelname}.cam.h1.0101-4012_chazhi.nc\" \
+       outputpath='"/home/yangsong3/data-observation/linshh/data/wc-result/"' \
+       outputname=\"msf_${modelname}_0101-4012.nc\" \
+     ./191209-mass_streamfunciton_cesm-F2000.ncl
+   echo "finish CESM mass_stream function"
+  fi
 
 
 # # # step4 calculate Local WC and HC
