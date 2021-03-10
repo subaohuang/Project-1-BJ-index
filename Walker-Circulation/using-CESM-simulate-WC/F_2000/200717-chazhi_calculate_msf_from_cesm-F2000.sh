2f78322842b82e0b14df46ea0500f2b15a6ff315
linshh <linshh55@mail2.sysu.edu.cn>
2021-03-11 04:03:09 +0800

    210311

--- Walker-Circulation/using-CESM-simulate-WC/F_2000/200717-chazhi_calculate_msf_from_cesm-F2000.sh
+++ Walker-Circulation/using-CESM-simulate-WC/F_2000/200717-chazhi_calculate_msf_from_cesm-F2000.sh
@@ -37,30 +37,30 @@ variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,TREFHT
 #     cdo_old select,name=${variable} ${modelname}.cam.h0.* ${modelname}.cam.h1.0101-4012.nc
 #   fi
 
-# step2 : interpolate the data from hybird level to pressure level
-  if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc ] ; then    ####判断差值的文件是否已经存在
-    echo "don't exit chazhi file, procecing..."
-    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
-    pwd
-    ncl  -nQ inpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc\" \
-         outpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc\" \
-       /home/ys17-19/lsh/Project/SCS-rain/annual/191209-CESM-data-chazhi.ncl
-    echo "finish CESM chazhi"
-  fi 
-# step3 calculate mass stream function
+# # step2 : interpolate the data from hybird level to pressure level
+#   if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc ] ; then    ####判断差值的文件是否已经存在
+#     echo "don't exit chazhi file, procecing..."
+#     cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
+#     pwd
+#     ncl  -nQ inpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc\" \
+#          outpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc\" \
+#        /home/ys17-19/lsh/Project/SCS-rain/annual/191209-CESM-data-chazhi.ncl
+#     echo "finish CESM chazhi"
+#   fi 
+# # step3 calculate mass stream function
 
-  if  [ ! -e /home/yangsong3/data-observation/linshh/data/wc-result/msf_${modelname}_0101-4012.nc ] ; then
-     ## 斜杠用来添加"，不然"会被默认成输出变量用的符号 
-   echo "don't exit msf file, procecing..."
-   cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
-   pwd
-   ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
-       filename=\"${modelname}.cam.h1.0101-4012_chazhi.nc\" \
-       outputpath='"/home/yangsong3/data-observation/linshh/data/wc-result/"' \
-       outputname=\"msf_${modelname}_0101-4012.nc\" \
-     ./191209-mass_streamfunciton_cesm-F2000.ncl
-   echo "finish CESM mass_stream function"
-  fi
+#   if  [ ! -e /home/yangsong3/data-observation/linshh/data/wc-result/msf_${modelname}_0101-4012.nc ] ; then
+#      ## 斜杠用来添加"，不然"会被默认成输出变量用的符号 
+#    echo "don't exit msf file, procecing..."
+#    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
+#    pwd
+#    ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
+#        filename=\"${modelname}.cam.h1.0101-4012_chazhi.nc\" \
+#        outputpath='"/home/yangsong3/data-observation/linshh/data/wc-result/"' \
+#        outputname=\"msf_${modelname}_0101-4012.nc\" \
+#      ./191209-mass_streamfunciton_cesm-F2000.ncl
+#    echo "finish CESM mass_stream function"
+#   fi
 
 
 # # # step4 calculate Local WC and HC
@@ -118,7 +118,7 @@ variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,TREFHT
    echo "finish CESM HC mass_stream function"
   fi
 
-   echo "finish this script"
+  echo "finish this script"
 #-----------------------------------------------------------
 
 
