1e2c9ee49675154b41c4ef60c9f87998320e25e8
linshh <linshh55@mail2.sysu.edu.cn>
2021-05-30 04:03:10 +0800

    210530

--- ENSO/CESM/B/210527-extract-cam-data.sh
+++ ENSO/CESM/B/210527-extract-cam-data.sh
@@ -31,47 +31,47 @@ variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,TREFHT,T
 #step1 : merge the cesm data into a whole data by using cdo
   ## the prefix of data is usually CESM compet name ,alarm for time select
 
-  if  [ ! -e  ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc ] ; then
+  if  [ ! -e  ${PRE_DIR_ORG}${modelname}.cam.h1.0251-0280.nc ] ; then
     echo "don't exit merge file, procecing..."
     cd $PRE_DIR_ORG
-    cdo_old select,name=${variable} ${modelname}.cam.h0.* ${modelname}.cam.h1.0101-4012.nc
+    cdo_old select,name=${variable} ${modelname}.cam.h0.* ${modelname}.cam.h1.0251-0280.nc
   fi
 
 # step2 : interpolate the data from hybird level to pressure level
-  if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc ] ; then    ####判断差值的文件是否已经存在
+  if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0251-0280_chazhi.nc ] ; then    ####判断差值的文件是否已经存在
     echo "don't exit chazhi file, procecing..."
     cd /home/ys17-19/lsh/Project/ENSO/CESM/B/
     pwd
-    ncl  -nQ inpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012.nc\" \
-         outpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc\" \
+    ncl  -nQ inpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0251-0280.nc\" \
+         outpath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0251-0280_chazhi.nc\" \
      ./210423-CESM-data-chazhi-finalver.ncl
     echo "finish CESM chazhi"
   fi 
 # # step3 calculate mass stream function
 
-#   if  [ ! -e /home/ys17-19/lsh/data/wc-result/msf_${modelname}_0101-4012.nc ] ; then
+#   if  [ ! -e /home/ys17-19/lsh/data/wc-result/msf_${modelname}_0251-0280.nc ] ; then
 #      ## 斜杠用来添加"，不然"会被默认成输出变量用的符号 
 #    echo "don't exit msf file, procecing..."
 #    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
 #    pwd
 #    ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
-#        filename=\"${modelname}.cam.h0.0101-4012_chazhi.nc\" \
+#        filename=\"${modelname}.cam.h0.0251-0280_chazhi.nc\" \
 #        outputpath='"~/lsh/data/wc-result/"' \
-#        outputname=\"msf_${modelname}_0101-4012.nc\" \
+#        outputname=\"msf_${modelname}_0251-0280.nc\" \
 #      ./191209-mass_streamfunciton_cesm-F2000.ncl
 #    echo "finish CESM mass_stream function"
 #   fi
 
 
 # # # step4 calculate Local WC and HC
-#    if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012_local_wk_hc_500.nc ] ; then    ####判断差值的文件是否已经存在
+#    if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h0.0251-0280_local_wk_hc_500.nc ] ; then    ####判断差值的文件是否已经存在
 #    echo "don't exit WC and HC file, procecing..."
 #    cd /home/ys17-19/lsh/Project/Walker-Circulation/using-CESM-simulate-WC/F_2000/
 #    pwd
 #    ncl -nQ inpath=\"${PRE_DIR_ORG}\"               \
-#        filename=\"${modelname}.cam.h0.0101-4012_chazhi.nc\" \
+#        filename=\"${modelname}.cam.h0.0251-0280_chazhi.nc\" \
 #        outputpath=\"${PRE_DIR_ORG}\"               \
-#        outputname=\"${modelname}.cam.h0.0101-4012_local_wk_hc_500.nc\" \
+#        outputname=\"${modelname}.cam.h0.0251-0280_local_wk_hc_500.nc\" \
 #      ./200902-local_wk_hc_cesm-F2000.ncl
 #    echo "finish CESM mass_stream function"
 #   fi
@@ -80,25 +80,25 @@ variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,TREFHT,T
 
 
 # # # step5 calculate vp and sf
-#    if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_vp_sf.nc ] ; then    ####判断差值的文件是否已经存在
+#    if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h1.0251-0280_vp_sf.nc ] ; then    ####判断差值的文件是否已经存在
 #    echo "don't exit vp and sf file, procecing..."
 #    cd /home/ys17-19/lsh/Project/SCS-rain/F2000_model_experiment/
 #    pwd
-#    ncl -nQ infilepath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_chazhi.nc\" \
-#        outfilepath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0101-4012_vp_sf.nc\"   \
+#    ncl -nQ infilepath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0251-0280_chazhi.nc\" \
+#        outfilepath=\"${PRE_DIR_ORG}${modelname}.cam.h1.0251-0280_vp_sf.nc\"   \
 #      ./200909-F2000-cal-write-era-velocity-potensial-streamfuc.ncl
 #    echo "finish CESM VP and SF calculate"
 #   fi
 
 # # # # step6 calculate vr and ur
-#    if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h0.0101-4012_ur_vr.nc ] ; then    ####判断差值的文件是否已经存在
+#    if [ ! -e ${PRE_DIR_ORG}${modelname}.cam.h0.0251-0280_ur_vr.nc ] ; then    ####判断差值的文件是否已经存在
 #    echo "don't exit rotation wind file, procecing..."
 #    cd /home/ys17-19/lsh/Project/SCS-rain/F2000_model_experiment/
 #    pwd
 #    ncl -nQ inpath=\"${PRE_DIR_ORG}\"                           \
-#         filename=\"${modelname}.cam.h1.0101-4012_chazhi.nc\" \
+#         filename=\"${modelname}.cam.h1.0251-0280_chazhi.nc\" \
 #         outputpath=\"${PRE_DIR_ORG}\"                          \
-#        outputname=\"${modelname}.cam.h1.0101-4012_ur_vr.nc\"   \
+#        outputname=\"${modelname}.cam.h1.0251-0280_ur_vr.nc\"   \
 #      ./201103-cal-F2000-ur-vr.ncl
 #    echo "finish CESM rotation wind calculate"
 #   fi
