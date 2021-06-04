c04c3f065597a0c7b348cf678a7a456596f2df3f
linshh <linshh55@mail2.sysu.edu.cn>
2021-06-05 04:03:08 +0800

    210605

--- ENSO/CESM/B/210527-extract-cam-data.sh
+++ ENSO/CESM/B/210527-extract-cam-data.sh
@@ -17,10 +17,10 @@
 # Path of the original data
 # Caution: DO NOT DELETE /" IN STRING!
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_IhPO/
-PRE_DIR_ORG=/home/yangsong3/CMIP6/linshh/CESM-data/B/lsh_B2000_WNP_heating_Idl/
+PRE_DIR_ORG=/home/yangsong3/CMIP6/linshh/CESM-data/B/lsh_B2000_WNP_heating_IDLthree/
 # PRE_DIR_ORG=/home/ys17-19/lsh/CESM-data/F/F_2000_GHG/
 STEP=3
-modelname=lsh_B2000_WNP_heating_Idl
+modelname=lsh_B2000_WNP_heating_IDLthree
 # modelname=F_2000_ghg
 # variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,UBOT,VBOT,TREFHT
 variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,TREFHT,T
