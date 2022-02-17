fcab1592625b980561ba756435eb1434b8ef5b10
linshh <linshh55@mail2.sysu.edu.cn>
2022-02-18 04:01:06 +0800

    220218

--- monsoon-ENSO/Springbarrier/SPB-monsoon/PREC_index/model_experiment/211031-extract-ocean-temp.sh
+++ monsoon-ENSO/Springbarrier/SPB-monsoon/PREC_index/model_experiment/211031-extract-ocean-temp.sh
@@ -22,41 +22,77 @@ do
 done
 }
 
-cd /home/yangsong3/data-model/CESM_CTRL/B2000_F19G16_CAM4_CTRL/pre/
+# Path of the original data
+# Caution: DO NOT DELETE /" IN STRING!
+modelname=year286_enso_wind_forcing_ctrl_5m
+# modelname=year286_enso_wind_forcing_heatsen_5m
+PRE_DIR_ORG=/home/yangsong3/CMIP6/linshh/CESM-data/B/enso_monsoon_ensemble/${modelname}/
+STEP=1
+# variable=U,V,OMEGA,PRECL,PRECC,PSL,PS,Z3,Q,UBOT,VBOT,TREFHT
+variable=TEMP
+# variable=hyam
+ensemble_start=1
+ensemble_end=30
+# year1=0257
+# year2=0258
 
-Var_Name_total=("TEMP")
-modelname=B2000_f19g16_CP_CTRL
+year1=0286
+year2=0287
 
-echo "********start  $Model_Name  *************************"
-OUT_DIR=/home/yangsong3/data-model/CESM_CTRL/B2000_F19G16_CAM4_CTRL/pre/
-IN_DIR=/home/yangsong3/data-model/CESM_CTRL/B2000_F19G16_CAM4_CTRL/pre/
+cd /home/yangsong3/CMIP6/linshh/CESM-data/B/B2000_f19g16_CTRL/
+cdo -s -showlevel pop.temp_upperlevel.0250-0300.nc > lelinfo.txt  ##print level info 
+cat lelinfo.txt | xargs -n1  > levinfo_new.txt ##convert to colum
 
+declare -a lev
+for line in `cat levinfo_new.txt`
+do 
+   lev[$c]=${line}","    ### 加逗号
+   ((c++))
+done 
 
-# ncl_filedump -v z_t  B2000_f19g16_CP_CTRL.pop.h.0268-11.nc > lelinfo.txt  ##print level info 
-# cat lelinfo.txt | xargs -n1  > levinfo_new.txt ##convert to colum
- 
+rm lelinfo.txt
+rm  levinfo_new.txt
 
-# declare -a lev
-# for line in `cat levinfo_new.txt`
-# do 
-#    lev[$c]=${line}","    ### 加逗号
-#    ((c++))
-# done 
-# rm lelinfo.txt
-# rm  levinfo_new.txt
+echo ${lev[*]} > test_lev.txt ##挑取0-400的深度
+
 # getArrItemIdx "${lev[*]}" 40000  ##调用寻找深度为400的位置的函数
 # ind=`echo $?`   ## 返回 functon return的值
 # ind=`expr $ind + 1`  ##多取小于400的下一层
 # echo ${lev[*]:0:$ind} > test_lev.txt ##挑取0-400的深度
-# levselect=`cat test_lev.txt | sed s/[[:space:]]//g` ###去掉空格
-# rm test_lev.txt
-# echo $levselect
-
+levselect=`cat test_lev.txt | sed s/[[:space:]]//g` ###去掉空格
+# exit
 #  z_t=500,1500,2500,3500,4500,5500,6500,7500,8500,9500,10500,11500,12500,13500,14500,15500,16509.84,17547.9,18629.13,19766.03,\
 # 20971.14,22257.83,23640.88,25137.02,26765.42,28548.37,30511.92,32686.8,35109.35,37822.76,40878.46
+z_t=$levselect
+echo $z_t
 
- z_t=165
 
-cdo  -select,name=${Var_Name_total},level=$z_t ${modelname}.pop.h.0{250..349}* ${modelname}.pop.temp_upperlevel.0250-0349nc
-        # rename .nc _2.5.nc $OUT_DIR/$file
+cd ${PRE_DIR_ORG}
+
+# module load cdo/1.9.9rc7-icc15
+if [ ${STEP} -eq 1 ]; then 
+# for ((i=1;i<$ensemble_num+1;i++)) ;
+for i in `seq $ensemble_start $ensemble_end`
+do 
+  echo ${i}
+#step1 : merge the cesm data into a whole data by using cdo
+  ## the prefix of data is usually CESM compet name ,alarm for time select
+  # if  [ -e ${PRE_DIR_ORG}Ensemble_fixinitial_${i}/${modelname}.fixinitial.sst.${year1}-${year2}.nc ] ; then
+  # rm ${PRE_DIR_ORG}Ensemble_${i}/${modelname}.upperlevel.temp.${year1}-${year2}.nc
+  # fi
+  if  [ ! -e  ${PRE_DIR_ORG}Ensemble_${i}/${modelname}.upperlevel.temp.${year1}-${year2}.nc ] ; then
+  	echo "ensemble '${i}'"
+    echo "don't exit merge file, procecing..."
+    cd ${PRE_DIR_ORG}Ensemble_${i}
+    cdo  -select,name=${variable},level=$z_t ${modelname}.pop.h.0* ${modelname}.upperlevel.temp.${year1}-${year2}.nc
+  fi
+done 
+fi
+
+# if [ ${STEP} -eq 2 ]; then 
+cd ${PRE_DIR_ORG}
+# rm ${modelname}.fixinitial.sst.${year1}-${year2}_ensemblemean.nc 
+# step3 : ensemble mean 
+cdo ensavg ${PRE_DIR_ORG}Ensemble_{1..30}/${modelname}.upperlevel.temp.${year1}-${year2}.nc  ${modelname}.upperlevel.temp.${year1}-${year2}_ensemblemean_1-30.nc
+
 echo "finish script"
